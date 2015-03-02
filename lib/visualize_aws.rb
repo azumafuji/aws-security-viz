09d776a638912b978996ceb9e70789829df82101
Dean Sellis <dean@trialreach.com>
2015-03-02 16:14:02 -0600

    Add options for filtering groups
    Added options to filter security groups by name.  Also added option to
    exclude egress rules and only show ingress rules.

--- lib/visualize_aws.rb
+++ lib/visualize_aws.rb
@@ -21,21 +21,23 @@ class VisualizeAws
     g = Graph.new
     @security_groups.each_with_index { |group, index|
       picker = ColorPicker.new(@options[:color])
-      g.add_node(group.name)
-      group.traffic.each { |traffic|
+      if group.name.match(@options[:group_name])
+       g.add_node(group.name)
+       group.traffic.each { |traffic|
         if traffic.ingress
           g.add_edge(traffic.from, traffic.to, :color => picker.color(index, traffic.ingress), :style => 'bold', :label => traffic.port_range)
-        else
+        elsif @options[:egress] 
           g.add_edge(traffic.to, traffic.from, :color => picker.color(index, traffic.ingress), :style => 'bold', :label => traffic.port_range)
         end
       }
+      end
     }
     g
   end
 
   def render(g, output_file)
     extension = File.extname(output_file)
-    g.output(extension[1..-1].to_sym => output_file, :use => 'fdp')
+    g.output(extension[1..-1].to_sym => output_file, :use => 'dot')
   end
 end
 
@@ -47,6 +49,8 @@ if __FILE__ == $0
     opt :source_file, 'JSON source file containing security groups', :type => :string
     opt :filename, 'Output file name', :type => :string, :default => 'aws-security-viz.png'
     opt :color, 'Colored node edges', :default => false
+    opt :group_name, 'Group name starts with', :type => :string, :default => ''
+    opt :egress, 'Show egress rules', :default => false
   end
   if opts[:source_file].nil?
     Trollop::die :access_key, 'is required' if opts[:access_key].nil?
